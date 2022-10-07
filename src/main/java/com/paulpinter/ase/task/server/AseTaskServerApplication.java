ccc25f949959280f4296ee9dd2c2ad4f2b4f8aea
Paul Pinter <paul.pinter@fastmail.com>
2022-10-07 15:35:00 +0200

    init

---
+++ src/main/java/com/paulpinter/ase/task/server/AseTaskServerApplication.java
@@ -0,0 +1,13 @@
+package com.paulpinter.ase.task.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseTaskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AseTaskServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulpinter/ase/task/server/AseTaskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulpinter.ase.task.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseTaskServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
